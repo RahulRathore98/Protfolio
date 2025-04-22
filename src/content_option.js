e6b23fc15464d782e6b5de7ad17550efb1351873
rahul <rahul@remotebricks.com>
2025-04-22 12:48:24 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,173 @@
+const logotext = "Rathore";
+const meta = {
+    title: "Rahul Rathore",
+    description: "I’m Rahul Rathore Frontend devloper,currently working in Remotebricks",
+};
+
+const introdata = {
+    title: "I’m Rahul Rathore",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web apps",
+    },
+    description: "I'm a frontend developer focused on building clean, responsive, and user-friendly web interfaces using React, TypeScript, and modern web technologies. I turn designs into seamless digital experiences.",
+    your_img_url: "./Rutik.png",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "Hey! I am Rahul Rathore and I am passionate to my work and I have Ability to grab more things and make my Company proud",
+
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "c++",
+        value: 65,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Html",
+        value: 100,
+    }, {
+        name: "Css",
+        value:100,
+    }, {
+        name: "Sass",
+        value:75
+    }
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Client Satifiscation is first Priority",
+    },
+    {
+        title: "Web Apps",
+        description: "Make Full functionationality",
+    },
+    {
+        title: "Figma",
+        description: "Still Working on Figma for more proficient",
+    },
+];
+
+const dataportfolio = [{
+        img: "./Rahul1.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul2.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul3.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul4.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul5.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul6.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "./Rahul7.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "./Rahul8.jpeg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    // {
+    //     img: "./Rahul1.jpeg",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "./Rahul1.jpeg",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "./Rahul1.jpeg",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rutikmaru8099@GMAIL.COM",
+    YOUR_FONE: "+91 9358926849",
+    description: "Every great idea starts with a conversation. Whether it's web development, design, or just some tech talk — I'm all ears. Reach out and let’s turn your vision into something real.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_mczpanc",
+    YOUR_TEMPLATE_ID: "template_k4592fo",
+    YOUR_USER_ID: "-OlVA-L575jfjgMym",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "www.linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
